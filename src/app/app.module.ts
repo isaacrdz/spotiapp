60ce923d94dc09cb6fb7b6f084b246889a013173
isaacrdz <zack_cpp@hotmail.com>
2017-11-11 22:01:13 -0600

    top tracks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
+import { ArtistComponent } from './components/artist/artist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    NavbarComponent
+    NavbarComponent,
+    SinfotoPipe,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -7,14 +7,16 @@ export class SpotifyService {
 
   artists:any [] = [];
 
-  baseURL:String = "https://api.spotify.com/v1/search"
+  baseURL:String = "https://api.spotify.com/v1/search";
+  artistURL:String = "https://api.spotify.com/v1/artists";
+
 
 
   constructor( private http:Http) { }
 
     getArtists(term:String){
       let headers = new Headers();
-      headers.append('authorization', 'Bearer BQCQ9PZrkrWiCRpSIwoFFrClFK8wff39p-BBOdjih-3pe-J2eHrxVfGTMOfNB90pG7OvLfFfbtQr0ZDlkY05Jg')
+      headers.append('authorization', 'Bearer BQB3XmzuqvFjP2h6ylLksPID8miH-0veexrQyTNDn2s_Ryf76tvNPm3Ye2pd3AUieSOhD3j5kBE4QQg9QNI7tA')
       let query = `?q=${term}&type=artist`
       let url = this.baseURL + query;
 
@@ -23,6 +25,32 @@ export class SpotifyService {
             // console.log(res.json().artists.items);
             this.artists = res.json().artists.items;
             console.log(this.artists);
+          })
+    }
+
+    getArtist(id:String){
+      let headers = new Headers();
+      headers.append('authorization', 'Bearer BQB3XmzuqvFjP2h6ylLksPID8miH-0veexrQyTNDn2s_Ryf76tvNPm3Ye2pd3AUieSOhD3j5kBE4QQg9QNI7tA')
+      let query = `/${id}`;
+      let url = this.artistURL + query;
+
+      return this.http.get(url, { headers })
+          .map(res=>{
+            console.log(res.json());
+            return res.json();
+          })
+    }
+
+    getTop(id:String){
+      let headers = new Headers();
+      headers.append('authorization', 'Bearer BQB3XmzuqvFjP2h6ylLksPID8miH-0veexrQyTNDn2s_Ryf76tvNPm3Ye2pd3AUieSOhD3j5kBE4QQg9QNI7tA')
+      let query = `/${id}/top-tracks?country=US`;
+      let url = this.artistURL + query;
+
+      return this.http.get(url, { headers })
+          .map(res=>{
+            console.log(res.json().tracks);
+            return res.json().tracks;
           })
     }
 }
